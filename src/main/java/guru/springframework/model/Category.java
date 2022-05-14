7790750a990daefad198b33261fa7949e1bed4d4
chetra_seng <sengchetra.love@icloud.com>
2022-05-14 21:42:21 +0700

    Added Exercise

---
+++ src/main/java/guru/springframework/model/Category.java
@@ -0,0 +1,48 @@
+package guru.springframework.model;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+
+    @ManyToMany(mappedBy = "categories")
+    private Set<Recipe> recipes;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Set<Recipe> getRecipes() {
+        return recipes;
+    }
+
+    public void setRecipes(Set<Recipe> recipes) {
+        this.recipes = recipes;
+    }
+}
